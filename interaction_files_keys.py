87fc16437168a890852e23976031fa84fb3a866e
D-Stiv <steveludovic.gounoue@mail.polimi.it>
2020-03-26 04:14:32 +0100

    keys used in interaction files

---
+++ interaction_files_keys.py
@@ -0,0 +1,17 @@
+# contains the keys used in the interactin json files
+
+# everything here is related to the field, so name is field_name, value is fiel_value
+
+zero = 'zero'
+one_name = 'one_name'
+one_value = 'one_value'
+two_names = 'two_names'
+two_values = 'two_values'
+name_value_name_value = 'name_value_name_value'
+name_value_value_name = 'name_value_value_name'
+value_name_value_name = 'value_name_value_name'
+value_name_name_value = 'value_name_name_value'
+name_value_value = 'name_value_value'
+value_name_value = 'value_name_value'
+value_name_name = 'value_name_name'
+name_value_name = 'name_value_name'
